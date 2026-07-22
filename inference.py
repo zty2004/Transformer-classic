2ee160e72c8b88b3c4953f4c765f17ce372a9952
tuf <zty13162909941@126.com>
2026-07-22 17:32:55 +0800

    feat(inference): LLaMA model

--- inference.py
+++ inference.py
@@ -54,11 +54,75 @@ class LLaMA:
 
         return LLaMA(model, tokenizer, model_args)
     
+    def text_completion(self, prompts: list[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
+        if max_gen_len is None:
+            max_gen_len = self.args.max_seq_len - 1
+        prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
+        batch_size = len(prompt_tokens)
+        assert batch_size <= self.args.max_batch_size
+        max_prompt_len = max(len(prompt) for prompt in prompts)
+        assert max_prompt_len <= self.args.max_seq_len
+        total_len = min(self.args.max_seq_len, max_gen_len + max_prompt_len)
+
+        # Create the list that will contain the generated tokens, along with the initial prompt tokens
+        pad_id = self.tokenizer.pad_id()
+        tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, decice = device)
+        for k, t in enumerate(prompt_tokens):
+            # Populate the initial tokens with the prompt tokens
+            tokens[k, :len(t)] = torch.tensor(t, dtype=torch.long, device = device)
+        
+        eos_reached = torch.Tensor([False] * batch_size, device = device)
+        prompt_tokens_mask = tokens != pad_id # prompt token -> True, otherwise -> False
+        for cur_pos in tqdm(range(1, total_len), desc='Generating tokens'):
+            with torch.no_grad():
+                logits = self.model.forward(tokens[:, cur_pos - 1:cur_pos], cur_pos)
+            if temperature > 0:
+                # Top P
+                probs = torch.softmax(logits[:, -1] / temperature, dim = -1)
+                next_token = self._sample_top_p(probs, top_p)
+            else:
+                # Greedy
+                next_token = torch.softmax(logits[:, -1], dim = -1)
+            
+            next_token = next_token.reshape(-1)
+            next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            tokens[:, cur_pos] = next_token
+            # EOS reached if we found an EOS token for a padding position
+            eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id())
+            if all(eos_reached):
+                break
+
+        out_tokens = []
+        out_text = []
+        for promt_index, current_prompt_tokens in enumerate(tokens.tolist()):
+            # Cut to EOS
+            if self.tokenizer.eos_id() in current_prompt_tokens:
+                eos_idx = current_prompt_tokens.index(self.tokenizer.eos_id())
+                current_prompt_tokens = current_prompt_tokens[:eos_idx]
+            out_tokens.append(current_prompt_tokens)
+            out_text.append(self.tokenizer.decode(current_prompt_tokens))
+        return (out_tokens, out_text)
+
+    def _sample_top_p(self, probs, p):
+        probs_sort, probs_idx = torch.sort(probs, dim = -1, descendings = True)
+        probs_sum = torch.cumsum(probs_sort, dim = -1)
+        mask = probs_sum - probs_sort > p
+        probs_sort[mask] = 0.0
+        probs_sort.div_(probs_sort.sum(div = -1, keepdim = True))
+        next_token = torch.multinomial(probs_sort, num_samples = 1)
+        next_token = torch.gather(probs_idx, -1, next_token)
+        return next_token
+
 if __name__ == '__main__':
     torch.manual_seed(0)
 
     allow_cuda = False
     device = 'cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'
+
+    prompts = [
+        "Tell me who is Tianyou Zuo:",
+        "What would happen if AI rules human?"
+    ]
 
     model = LLaMA.build(
         chekpoints_dir = 'llama-2-7b/',
@@ -69,4 +133,8 @@ if __name__ == '__main__':
         device = device
     )
 
-    print('All OK')
+    # Inference
+    out_tokens, out_text = (model.text_completion(prompts, max_gen_len = 64))
+    for i in range(len(out_text)):
+        print(f'{out_text[i]}')
+        print('-' * 50)
