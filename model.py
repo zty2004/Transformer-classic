0a85e0b7c38e5196d8ad2c2d30a3665ef196ce57
tuf <zty13162909941@126.com>
2026-07-22 15:44:23 +0800

    fix(model): fix typo

--- model.py
+++ model.py
@@ -181,7 +181,7 @@ class FeedForward(nn.Module):
         if args.ffn_dim_multiplier is not None:
             hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
         # ceil to the nearest multiple of args.multiple_of
-        hidden = args.multiple_of * ((hidden + args.multiple_of - 1) // args.multiple_of)
+        hidden = args.multiple_of * ((hidden_dim + args.multiple_of - 1) // args.multiple_of)
 
         self.w1 = nn.Linear(args.dim, hidden_dim, bias = False)
         self.w2 = nn.Linear(hidden_dim, args.dim, bias = False)
@@ -193,7 +193,6 @@ class FeedForward(nn.Module):
         x = swish * x_V
         x = self.w2(x)
         return x
-         
 
 class Transformer(nn.Module):
 
